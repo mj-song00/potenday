9e9cb4cb0278387a869a49dde5c7db0997b92407
mj-song00 <niveus128@gmail.com>
2024-01-29 13:40:29 +0900

    Remove: type 폴더 삭제

--- src/type/custom.d.ts
+++
@@ -1,7 +0,0 @@
-import { User } from 'src/users/entities/user.entity'; // 실제 사용자 엔터티의 경로로 변경
-
-declare module 'express' {
-  interface Request {
-    user?: User; // 여기에서 'User' 대신 실제 사용자 엔터티의 타입을 지정
-  }
-}
