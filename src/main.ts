539d250f3db6faa8a084d00017225e253f63252e
mj-song00 <niveus128@gmail.com>
2024-01-30 21:00:57 +0900

    Fix: CORS 수정

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const port = process.env.PORT;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
   });
   app.useGlobalPipes(new ValidationPipe());
 
