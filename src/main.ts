6a78610ca7d107131b3e1a2a608e37d29069778c
mj-song00 <niveus128@gmail.com>
2024-01-30 19:32:49 +0900

    Fix: cors fix

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port);
 }
