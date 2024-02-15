8e9d96bdd2833036f7fd66bcbe95a1666716e476
mj-song00 <niveus128@gmail.com>
2024-02-15 10:12:20 +0900

    Chore: main.ts https설정 수정

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 const port = process.env.PORT;
-const httpsOptions = {
-  key: fs.readFileSync('/etc/letsencrypt/live/picture-diary.site/privkey.pem'),
-  cert: fs.readFileSync(
-    '/etc/letsencrypt/live/picture-diary.site/fullchain.pem',
-  ),
-};
+// const httpsOptions = {
+//   key: fs.readFileSync('/etc/letsencrypt/live/picture-diary.site/privkey.pem'),
+//   cert: fs.readFileSync(
+//     '/etc/letsencrypt/live/picture-diary.site/fullchain.pem',
+//   ),
+// };
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    httpsOptions,
+    //  httpsOptions,
   });
   app.enableCors({
     credentials: true,
