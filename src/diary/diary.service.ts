8533924cf7a4038e04a5d4c32332bba5b61906a8
mj-song00 <niveus128@gmail.com>
2024-02-16 02:12:24 +0900

    Chore: createDiary API 수정완료

--- src/diary/diary.service.ts
+++ src/diary/diary.service.ts
@@ -67,9 +67,7 @@ export class DiaryService {
       user,
       imageUrl: imageUrl.url,
     });
-    console.log(createDiary);
 
-    console.log(await this.diaryRepository.save(createDiary));
     const diary = await this.diaryRepository.save(createDiary);
     return { result: 'success' };
   }
