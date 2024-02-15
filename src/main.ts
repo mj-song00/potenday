929bb0753a277b828f3edc6f9a305a3111d99ccf
mj-song00 <niveus128@gmail.com>
2024-02-15 09:53:54 +0900

    Chore: main.ts https설정

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,28 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as fs from 'fs';
 const port = process.env.PORT;
+const httpsOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/picture-diary.site/privkey.pem'),
+  cert: fs.readFileSync(
+    '/etc/letsencrypt/live/picture-diary.site/ullchain.pem',
+  ),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.enableCors({
+    credentials: true,
     origin: 'http://localhost:5173',
   });
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   await app.listen(port);
 }
