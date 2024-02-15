36471b81de5ae17fc9ba3752a2ad14add001490a
mj-song00 <niveus128@gmail.com>
2024-02-15 15:46:11 +0900

    Chore: CI/CD yml파일 수정

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Picture-Diary Server!';
+    return '왜 npm run build가 안되는거지';
   }
 }
