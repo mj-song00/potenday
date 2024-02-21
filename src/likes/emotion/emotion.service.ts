a43a981cb1815ca587f8945af034178f4b9f204c
mj-song00 <niveus128@gmail.com>
2024-02-21 16:50:40 +0900

    Chore: checkEmotion API 수정

--- src/likes/emotion/emotion.service.ts
+++ src/likes/emotion/emotion.service.ts
@@ -56,7 +56,7 @@ export class EmotionService {
       .createQueryBuilder('emotion')
       .where('emotion.userId = :userId', { userId: user.id })
       .andWhere('emotion.diaryId = :diaryId', { diaryId: diaryId })
-      .getMany();
+      .getRawMany();
 
     // 감정을 나타내는 속성과 해당하는 boolean 값을 설정하기 위한 객체를 정의합니다.
     const emotionsData: { [key: string]: boolean } = {
