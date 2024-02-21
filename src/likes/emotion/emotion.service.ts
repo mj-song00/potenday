113d303f135331dfe9edf7e2fe66c366a6dfa784
mj-song00 <niveus128@gmail.com>
2024-02-21 17:20:35 +0900

    Chore: checkEmotion API 수정

--- src/likes/emotion/emotion.service.ts
+++ src/likes/emotion/emotion.service.ts
@@ -52,13 +52,11 @@ export class EmotionService {
   }
 
   async checkEmotions(diaryId: number, user: UserEntity) {
-    const emotions = await this.emotionRepository.find({
-      select: {
-        userId: true,
-        diaryId: true,
-      },
-    });
-
+    const emotions = await this.emotionRepository
+      .createQueryBuilder('emotion')
+      .where('emotion.userId = :userId', { userId: user.id })
+      .andWhere('emotion.diaryId = :diaryId', { diaryId: diaryId })
+      .getMany();
     console.log(emotions);
 
     // 감정을 나타내는 속성과 해당하는 boolean 값을 설정하기 위한 객체를 정의합니다.
