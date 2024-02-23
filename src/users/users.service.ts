11a3a5ccd229494830785ef5b11d25939accd730
mj-song00 <niveus128@gmail.com>
2024-02-24 00:43:06 +0900

    Chore: Kakao service unlink 수정중

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -151,10 +151,10 @@ export class UsersService {
 
   async deleteUser(user: UserEntity) {
     const kakaoId = await this.kakaoService.unlink(user.kakaoId);
+    console.log(kakaoId);
+    // const deleteUser = await this.userRepository.delete(kakaoId);
 
-    const deleteuser = await this.userRepository.delete({ kakaoId });
-
-    return { result: 'success' };
+    // return { result: 'success' };
   }
 
   async getMe(user: UserEntity) {
