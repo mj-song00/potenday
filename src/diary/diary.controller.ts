4504c1d7bfcc5415317ac43ff3f334efe9c78055
cjy00n <ej921004@naver.com>
2024-03-15 12:43:51 +0900

    Chore : 이미지 생성 내용 수정

--- src/diary/diary.controller.ts
+++ src/diary/diary.controller.ts
@@ -36,9 +36,9 @@ export class DiaryController {
   @Post('generate-image')
   @Roles(ROLE.USER)
   async generateImage(@Body('input') input: string) {
-    const negativePrompt = `text, dirty, scared, ugly, sordid, gloomy`;
-    const context = `${input}, It looks like it was drawn with crayons by a child under 7 years old, 
-    naturally, only pictures`;
+    const negativePrompt = `any text, dirty, scared, ugly, sordid, gloomy`;
+    const context = `${input}, naturally`;
+
     try {
       const response = await this.diaryService.createImage(
         context,
