2bc2fa68eed4e520f6c4ff9eb9d78117b2a18361
mj-song00 <niveus128@gmail.com>
2024-03-20 16:53:18 +0900

    Comment: main.ts 주석 삭제

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,15 @@ import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
 const port = process.env.PORT;
-// const httpsOptions = {
-//   key: fs.readFileSync('/etc/letsencrypt/live/picture-diary.site/privkey.pem'),
-//   cert: fs.readFileSync(
-//     '/etc/letsencrypt/live/picture-diary.site/fullchain.pem',
-//   ),
-//   passphrase: process.env.PASS_KEY,
-// };
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    // httpsOptions,
-  });
+  const app = await NestFactory.create(AppModule, {});
   app.enableCors({
     credentials: true,
     origin: '*',
   });
-  app.useGlobalPipes(
-    new ValidationPipe({
-      // whitelist: true,
-      // transform: true,
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe({}));
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
   await app.listen(port);
